ac6d2eb0d27b373e9b9c2b513ea9b4ff50ba90a5
DEVitor0 <devitormoreira@gmail.com>
2025-03-24 12:11:51 -0300

    project documentation

---
+++ turnLed.asm
@@ -0,0 +1,6 @@
+; File: piscaled.asm
+; Author: DEVitor0 (https://github.com/DEVitor0)
+; Date: 03/24/25
+; Description: Simple virtual LED blink program for Linux x86_64
+; Build: nasm -f elf64 piscaled.asm && ld piscaled.o -o piscaled
+; Usage: ./piscaled (Ctrl+C to exit)
