ef4a8acb92809882572eeb2499cd4644a0ca6e43
DEVitor0 <devitormoreira@gmail.com>
2025-03-24 12:29:33 -0300

    declaring program variables

--- blinking.asm
+++ blinking.asm
@@ -4,3 +4,8 @@
 ; Description: Simple virtual LED blink program for Linux x86_64
 ; Build: nasm -f elf64 piscaled.asm && ld piscaled.o -o piscaled
 ; Usage: ./piscaled (Ctrl+C to exit)
+
+section .data
+    led_on    db  '[X]', 0xA
+    led_off   db  '[ ]', 0xA
+    clear     db  0x1B, '[2J', 0x1B, '[H', 0
